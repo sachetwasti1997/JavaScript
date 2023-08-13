be3e4379f9f18d4d7d11d7cc98d07e0e04252bf2
sachetwasti1997 <sachetwasti61@gmail.com>
2023-08-13 06:43:29 +0530

    used setters and getters in method

--- oop/starter/settergetters.js
+++ oop/starter/settergetters.js
@@ -15,3 +15,27 @@ console.log(account.latest);
 
 account.latest = 550;
 console.log(account.movements)
+
+class Car {
+    constructor(carName, carNumber) {
+        this.carName = carName;
+        this.carNumber = carNumber;
+    }
+
+    consoleCar() {
+        console.log(this)
+    }
+
+    set carmodel(carModel) {
+        this.carModel = carModel;
+    }
+
+    get carmodel() {
+        return this.carModel;
+    }
+}
+
+const car = new Car("BMW", "BLR3324");
+car.consoleCar();
+car.carmodel = "BMW";
+console.log(car.carmodel)
